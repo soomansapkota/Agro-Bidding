b7d1075197141617941c488a6e63b58c6e44999e
Suman Sapkota <suman.191443@ncit.edu.np>
2023-12-08 14:52:53 +0545

    Captcha Fixed

--- src/main/java/com/demo/utils/VerifyRecaptcha.java
+++ src/main/java/com/demo/utils/VerifyRecaptcha.java
@@ -18,7 +18,7 @@ import javax.net.ssl.HttpsURLConnection;
 public class VerifyRecaptcha {
 
 	public static final String url = "https://www.google.com/recaptcha/api/siteverify";
-	public static final String secret = "6Lduld0mAAAAAALooEAfLSMLMiROBu3wfGPmrzh0";
+	public static final String secret = "";
 	private final static String USER_AGENT = "Mozilla/5.0";
 
 	public static boolean verify(String gRecaptchaResponse) throws IOException {
